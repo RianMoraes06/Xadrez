fa66ec2436d71bb626d7a83492e39fa5dda34df7
Rian Moraes <rian-moraes@hotmail.com>
2023-06-13 17:51:55 -0300

    Criando a classe PosicaoXadrez

--- xadrez-console/tabuleiro/TabuleiroException.cs
+++ xadrez-console/tabuleiro/TabuleiroException.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace xadrez_console.tabuleiro
+namespace tabuleiro
 {
-    internal class TabuleiroException
+    internal class TabuleiroException : Exception
     {
+        public TabuleiroException(string msg) : base(msg)
+        {
+
+        }
     }
 }
